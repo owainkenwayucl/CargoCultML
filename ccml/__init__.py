8c0fb9f211d8540238dd3d368e0e2ed69bd19631
Owain Kenway <o.kenway@ucl.ac.uk>
2018-08-27 22:40:12 +0100

    Initial attempt at tag field parsing.

--- ccml/__init__.py
+++ ccml/__init__.py
@@ -2,6 +2,7 @@
 # limit memory blow-up attacks
 MAX_DEPTH=4096
 UNLIMITED_DEPTH=False
+TAG_NAME="CCML_TAG_NAME"
 
 # Check to see if current depth is OK.
 def depth_ok(depth): 
@@ -23,7 +24,7 @@ def split_out_tag(s, depth=1):
 # Do a right split to see if there's text after the tag.
                 right_split = left_split[1].split(">", 1)
 # Append the tag to ret_val.
-                tag = right_split[0]
+                tag = process_tag(right_split[0])
                 ret_val.append(tag)
 # If there's left over text append it.
                 if (len(right_split[1]) > 0):
@@ -37,4 +38,40 @@ def split_out_tag(s, depth=1):
     else:
         print("CCML: Not parsing as MAX_DEPTH exceeded.")
         ret_val.append(s)
+    return ret_val
+
+# Convert the string representaton of the contents of a tag into a dict
+# with:
+# TAG_NAME = tag 
+# subsequent fields as in the xml
+def process_tag(t):
+    ret_val = {}
+# Default to tag = t (for tags with no fields)
+    tag = t
+# If it has spaces, it has fields.
+    if " " in t:
+        tag = t.split(" ", 1)[0]
+        rest = t.split(" ",1)[1]
+# If there are = there are still fields left to process in the rest.
+        while "=" in rest:
+            var = rest.split("=", 1)[0].strip()
+# if a field name is the same as TAG_NAME append a _
+            if (var == TAG_NAME):
+                var = TAG_NAME + "_"
+            rest = rest.split("=",1)[1].strip()
+# Default to val of var is the rest of the text.
+            val = rest
+# Check to see if that's not true:
+            if (len(val) > 0):
+                if (val[0] == "'"):
+                    val = rest.split("'",2)[1]
+                    rest = rest.split("'",2)[2].strip()
+                elif (val[0] == '"'):
+                    val = rest.split('"',2)[1]
+                    rest = rest.split('"',2)[2].strip()
+                elif (" ") in rest:
+                    val = rest.split(" ",1)[0]
+                    rest = rest.split(" ",1)[1].strip()
+            ret_val[var] = val
+    ret_val[TAG_NAME] = tag
     return ret_val
